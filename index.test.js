bd24a51fa93107a957fd8ebb13c033926c93b032
Dharmik Savaliya <danny@Dharmiks-MacBook-Pro.local>
2021-09-15 12:27:46 -0700

    even number function and tests

--- index.test.js
+++ index.test.js
@@ -134,6 +134,12 @@ describe('[Exercise 6] Car', () => {
 })
 
 describe('[Exercise 7] isEvenNumberAsync', () => {
-  // test('[19] resolves true if passed an even number', () => {})
-  // test('[20] resolves false if passed an odd number', () => {})
+  test('[19] resolves true if passed an even number', async () => {
+    const result = await utils.isEvenNumberAsync(23456)
+    expect(result).toBe(true)
+  })
+  test('[20] resolves false if passed an odd number', async () => {
+    const result = await utils.isEvenNumberAsync(234563)
+    expect(result).toBe(false)
+  })
 })
